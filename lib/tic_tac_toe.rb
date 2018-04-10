080f976d57b7d3c3400e83575595cd035b12f5c5
Richard Fenix <richard.fenix@gmail.com>
2018-04-10 23:06:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,6 +74,16 @@ class TicTacToe
   def draw?
     full? && !won?
   end
+  
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if winning_combo = won?
+      @board[winning_combo.first]
+    end
+  end
 
 
   
