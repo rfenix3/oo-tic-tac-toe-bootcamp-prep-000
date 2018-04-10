83bc8a96789df9e0b7b98ba4529daacc54d4b420
Richard Fenix <richard.fenix@gmail.com>
2018-04-10 22:55:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,6 +42,11 @@ class TicTacToe
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
   
   
   def turn
