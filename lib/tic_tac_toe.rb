bb0591a4302b8b4b88f8261ef9cc13984ea93965
Richard Fenix <richard.fenix@gmail.com>
2018-04-10 23:08:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,7 +85,16 @@ class TicTacToe
     end
   end
 
-
+  def play
+    while !over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
   
 end
 
